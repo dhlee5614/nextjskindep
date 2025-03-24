7a6264e626d57195a9e78edfe0e6a6bb815d88f1
Dong Ho Lee <dhlee5614@gmail.com>
2025-03-24 19:57:04 +0900

    first source code

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="text-center pt-32 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">Welcome to my blog</h1>
+      <p className="max-w-[750px] mx-auto leading-8">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi distinctio deleniti, voluptate sequi soluta iusto numquam nulla architecto illum maiores aut dolorem repellendus in dolorum totam voluptas? Iure, dolor. Officia possimus magni, natus quas id excepturi vel saepe quis non.</p>
+    </main>
+  );
+}
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <main className="text-center pt-32 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">All posts</h1>
+    </main>
+  )
+}
+
+export default Page
